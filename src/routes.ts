129d0614254e063b90572cafef2505ab3f141f75
Matheus2798 <matheus.s.f@outlook.com.br>
2021-11-05 23:27:32 -0300

    feat: getOneUser route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
 import { finishedTask, getTasks } from './controller/TasksController';
 import { getOneTask } from './controller/TasksController';
@@ -7,6 +7,7 @@ import { updateTask } from './controller/TasksController';
 import { deleteTask } from './controller/TasksController';
 import { authUser, saveUser } from './controller/authController';
 import tokenValidator from './middleware/TokenValidator';
+import { getOneUser } from './controller/UserController';
 
 const routes = Router()
 
@@ -15,6 +16,7 @@ routes.post('/auth/create', saveUser)
 
 routes.use(tokenValidator)
 
+routes.get('/user', getOneUser)
 routes.get('/tasks', getTasks)
 routes.get('/tasks/:id', getOneTask)
 routes.post('/tasks', saveTasks)
